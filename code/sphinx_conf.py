b05659d9b905e02cd74dcef3771a478158955a30
Alex Kaszynski <akascap@gmail.com>
2022-11-05 13:05:11 -0600

    final edits

---
+++ code/sphinx_conf.py
@@ -0,0 +1,8 @@
+.. jupyter-execute::
+
+    from pyvista import examples
+    mesh = examples.download_st_helens()
+    warped = mesh.warp_by_scalar('Elevation')
+    surf = warped.extract_surface().triangulate()
+    surf = surf.decimate_pro(0.75)
+    surf.plot(cmap='gist_earth')
